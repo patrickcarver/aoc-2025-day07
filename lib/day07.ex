380348d09d32465ada58801e50a4a84d396b67dd
Patrick Carver <patrickcarver@gmail.com>
2025-12-10 10:48:14 -0600

    finished. added test

--- lib/day07.ex
+++ lib/day07.ex
@@ -1,30 +1,42 @@
 defmodule Day07 do
   def part1(file_name) do
-    [{start_x, start_y} | splitters] = load_tachyon_manifold(file_name)
-    beams = MapSet.new([{start_x, start_y + 1}])
+    {start, splitters_by_row} = tachyon_manifold_data(file_name)
+    beams = MapSet.new([start])
 
-    splitters
-    |> splitters_by_row()
-    |> shine_beam(beams, 0)
+    count_beam_splits(splitters_by_row, beams, 0)
   end
 
   def part2(file_name) do
-    file_name
+    {start, splitters_by_row} = tachyon_manifold_data(file_name)
+    beams = %{start => 1}
+
+    count_timelines(splitters_by_row, beams)
   end
 
-  defp splitters_by_row(splitters) do
-    splitters
-    |> Enum.group_by(fn {_x, y} -> y end)
-    |> Enum.to_list()
-    |> Enum.sort_by(fn {row, _} -> row end)
-    |> Enum.map(fn {_, list} -> list end)
+  defp count_timelines([], beams) do
+    Enum.sum_by(beams, fn {_key, value} -> value end)
   end
 
-  defp shine_beam([], _beams, num_splits) do
+  defp count_timelines([row | rest], beams) do
+    new_beams =
+      Enum.reduce(beams, %{}, fn {{x, y}, count}, acc ->
+        if {x, y + 1} in row do
+          acc
+          |> Map.update({x - 1, y + 2}, count, &(&1 + count))
+          |> Map.update({x + 1, y + 2}, count, &(&1 + count))
+        else
+          Map.update(acc, {x, y + 2}, count, &(&1 + count))
+        end
+      end)
+
+    count_timelines(rest, new_beams)
+  end
+
+  defp count_beam_splits([], _beams, num_splits) do
     num_splits
   end
 
-  defp shine_beam([row | rest], beams, num_splits) do
+  defp count_beam_splits([row | rest], beams, num_splits) do
     down_one_set = beams |> Enum.map(fn {x, y} -> {x, y + 1} end) |> MapSet.new()
     row_set = MapSet.new(row)
 
@@ -43,22 +55,41 @@ defmodule Day07 do
     unsplit_beams = difference |> Enum.map(fn {x, y} -> {x, y + 1} end) |> MapSet.new()
     new_beams = MapSet.union(split_beams, unsplit_beams)
 
-    shine_beam(rest, new_beams, num_splits + additional_num_splits)
+    count_beam_splits(rest, new_beams, num_splits + additional_num_splits)
   end
 
-  def load_tachyon_manifold(file_name) do
+  defp splitters_by_row(splitters) do
+    splitters
+    |> Enum.group_by(fn {_x, y} -> y end)
+    |> Enum.to_list()
+    |> Enum.sort_by(fn {row, _} -> row end)
+    |> Enum.map(fn {_, list} -> list end)
+  end
+
+  defp tachyon_manifold_data(file_name) do
+    file_name
+    |> lines()
+    |> tachyon_manifold()
+    |> prep_data()
+  end
+
+  defp prep_data([{first_x, first_y} | splitters]) do
+    {{first_x, first_y + 1}, splitters_by_row(splitters)}
+  end
+
+  defp tachyon_manifold(lines) do
+    for {line, y} <- Enum.with_index(lines),
+        {char, x} <- Enum.with_index(line),
+        char != ".",
+        into: [] do
+      {x, y}
+    end
+  end
+
+  defp lines(file_name) do
     file_name
     |> File.stream!()
     |> Stream.map(&String.trim_trailing/1)
     |> Stream.map(&String.graphemes/1)
-    |> Enum.with_index()
-    |> then(fn indexed_lines ->
-      for {line, y} <- indexed_lines,
-          {char, x} <- Enum.with_index(line),
-          char != ".",
-          into: [] do
-        {x, y}
-      end
-    end)
   end
 end
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -1,8 +1,13 @@
 defmodule Day07Test do
   use ExUnit.Case
-  doctest Day07
 
-  test "greets the world" do
-    assert Day07.hello() == :world
+  @test_file "priv/test.txt"
+  @input_file "priv/input.txt"
+
+  test "ensuring outputs return correct value" do
+    assert Day07.part1(@test_file) == 21
+    assert Day07.part1(@input_file) == 1658
+    assert Day07.part2(@test_file) == 40
+    assert Day07.part2(@input_file) == 53_916_299_384_254
   end
 end
